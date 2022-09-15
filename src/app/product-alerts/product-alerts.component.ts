c2731bbba768544e182f957242b05d96fbd09cab
RobertMarin <106458528+RobertMarin@users.noreply.github.com>
2022-09-14 20:40:50 -0700

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,14 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
-export class ProductAlertsComponent  {
-
+export class ProductAlertsComponent {
   @Input() product!: Product;
-
-
 }
